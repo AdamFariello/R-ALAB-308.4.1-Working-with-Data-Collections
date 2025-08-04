9e5ee30650a76b65b24b8437024169e9d5d5155f
AdamFariello <ajf270@scarletmail.rutgers.edu>
2025-08-04 15:37:40 -0400

    Reorganized code, plus properly solved first part of part 4

--- answer.js
+++ answer.js
@@ -1,30 +1,5 @@
 //TODO: Part1
-
-//Original code
-//Part 3: Feeling Loopy
-function part3OldCode() {
-		/*
-		let testString = 
-		`Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n`
-		+`1,0.00,0.050,0.050\n`
-		+ `2,0.49,0.066,0.066\n`
-		+ `3,0.98,0.087,0.080\n`
-		+ `4,1.47,0.116,0.108\n`
-		+ `5,1.96,0.142,0.138\n`
-		+ `6,2.45,0.166,0.158\n`
-		+ `7,2.94,0.193,0.174\n`
-		+ `8,3.43,0.204,0.192\n`
-		+ `9,3.92,0.226,0.205\n`
-		+ `10,4.41,0.238,0.232`
-		*/
-
-		let testString = 
-		`ID,Name,Occupation,Age\n`
-		+ `42,Bruce,Knight,41\n`
-		+ `57,Bob,Fry Cook,19\n`
-		+ `63,Blaine,Quiz Master,58\n`
-		+ `98,Bill,Doctor’s Assistant,26`
-
+function convStringToArrays(testString) {
 		let rows = []
 		let row = []
 		let s = ""
@@ -50,31 +25,93 @@ function part3OldCode() {
 		return rows
 }
 
+
 // New Code
-// Nothing? 
-// I accidentally used arrays last time, so this time it's already done.
+function part1() {
+	//I guess nothing to do?
+	let testString = 
+	`Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n`
+	+`1,0.00,0.050,0.050\n`
+	+ `2,0.49,0.066,0.066\n`
+	+ `3,0.98,0.087,0.080\n`
+	+ `4,1.47,0.116,0.108\n`
+	+ `5,1.96,0.142,0.138\n`
+	+ `6,2.45,0.166,0.158\n`
+	+ `7,2.94,0.193,0.174\n`
+	+ `8,3.43,0.204,0.192\n`
+	+ `9,3.92,0.226,0.205\n`
+	+ `10,4.41,0.238,0.232`
+
+	convStringToArrays(testString)
+}
+console.log("Part 1")
+part1()
 
 
 
 // TODO: Part 2
-//I guess also nothing? Just refer to old code
+function part2() {
+	//I guess also nothing? Just refer to old code
+}
+console.log("Part 2")
+part2()
 
 
 
 // TODO; Part 3
-let csv = part3OldCode()
-let arrOfObj = []
-let csvheader = csv[0]
-let csvbody = csv.slice(1)
-for (row of csvbody) {
-	let obj = {}
-	for (let rowIndex = 0; rowIndex < row.length; rowIndex++) {
-		obj[csvheader[rowIndex]] = row[rowIndex]
+function part3() {
+	let testString = 
+	`ID,Name,Occupation,Age\n`
+	+ `42,Bruce,Knight,41\n`
+	+ `57,Bob,Fry Cook,19\n`
+	+ `63,Blaine,Quiz Master,58\n`
+	+ `98,Bill,Doctor’s Assistant,26`
+	let csv = convStringToArrays(testString)
+	
+	let arrOfObj = []
+	let csvheader = csv[0]
+	let csvbody = csv.slice(1)
+	for (row of csvbody) {
+		let obj = {}
+		for (let rowIndex = 0; rowIndex < row.length; rowIndex++) {
+			obj[csvheader[rowIndex]] = row[rowIndex]
+		}
+		arrOfObj.push(obj)
 	}
-	arrOfObj.push(obj)
+	
+	for (obj of arrOfObj) {
+		console.log(obj)
+	}
+
+	return arrOfObj 
 }
+console.log("Part 3")
+let arrOfObj = part3()
 
 
-for (obj of arrOfObj) {
-	console.log(obj)
+// TODO: Part 4
+// TODO: Figure out if method chaining is a thing
+function part4(arrOfObj) {
+	//Remove last entry
+	arrOfObj = arrOfObj.slice(0,-1)
+	console.log(arrOfObj )
+	
+	/*
+	//insert at entry 1
+	let newObj = {
+		id: "48", 
+		name: "Barry", 
+		occupation: "Runner", 
+		age: "25" 
+	}
+	rightSide = arrOfObj.splice(1)
+	console.log(rightSide)
+	console.log(arrOfObj)
+
+	arrOfObj.append(newObj)
+	arrOfObj.append(rightSide)
+	console.log(arrOfObj)
+	*/
 }
+console.log("Part 4")
+part4(arrOfObj)
